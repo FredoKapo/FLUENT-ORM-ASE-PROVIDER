a02df6412e0541d628c06c31c91f35464b904ff7
FredoKapo <fredokapo@free.fr>
2015-08-03 12:04:52 +0200

    Update SybaseAdonetProvider.cs

--- SybaseAdonetProvider.cs
+++ SybaseAdonetProvider.cs
@@ -1,3 +1,25 @@
+//-------------------------------------------------------------------------------
+// <copyright file="SybaseAdonetProvider.cs" company="Dupleix-101">
+//   Copyright (c) 2015 Dupleix-101 
+//   Contribution project CODEPLEX Fluent-ORM
+//   Authors: Frederic DESSALLIEN (fred.dessallien@gmail)
+//           
+//   Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+//   you may not use this file except in compliance with one of the Licenses.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//   or
+//       http://www.microsoft.com/opensource/licenses.mspx
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------
+
 public class SybaseAdonetProvider : IDbProvider
     {
         private static readonly Lazy<DbProviderFactory> _dbProviderFactory = new Lazy<DbProviderFactory>(CreateDbProviderFactory, true);
